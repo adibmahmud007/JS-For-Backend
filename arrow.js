64b4cb906dd708b2ae694c032e159f68e0e54e91
adibmahmud007 <adibmahmud307@gmail.com>
2024-11-02 21:31:04 +0600

    arrow funtion in es6

---
+++ arrow.js
@@ -0,0 +1,19 @@
+/**
+ * Arrow function is the shortcut of the actual function
+ * we can write the arrow function in the followin steps
+ */
+
+const add=(num1,num2)=> num1+num2;
+
+ const sum=add(2,3);
+ console.log(sum);
+
+ //We can write this arrow func in normal way like 
+
+ function add2(num1,num2){
+    return num1+num2;
+
+ }
+ const sum2= add2(3,4);
+ console.log(sum2);
+
