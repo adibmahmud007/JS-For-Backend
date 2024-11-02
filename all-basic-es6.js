802a021b2a3411e15198a1d0132e2dbbe1bc4ede
adibmahmud007 <adibmahmud307@gmail.com>
2024-11-02 11:39:41 +0600

    Let and const difference

---
+++ all-basic-es6.js
@@ -0,0 +1,12 @@
+// let in js
+// we can change the value assigned to a let type
+
+let a=25;
+a=50;
+console.log(a)
+
+//but in constant we cant assign a value to an existing variable like let
+
+const b=20;
+// b=11; this is not possible
+console.log(b)
